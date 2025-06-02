dd0d8c4b76078104629229591ec03c32f572cb79
AnujKafle06 <153058470+AnujKafle06@users.noreply.github.com>
2025-06-02 09:51:38 +0545

    Fix: database

--- project/settings.py
+++ project/settings.py
@@ -85,16 +85,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-DATABASES ={
-    'default' : dj_database_url.parse(DATABASE_URL)
-}
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+# DATABASE_URL = os.getenv("DATABASE_URL")
+# DATABASES ={
+#     'default' : dj_database_url.parse(DATABASE_URL)
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
